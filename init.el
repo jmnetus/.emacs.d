fb8bf7a71d401a959ed8e396d34381dca9866a3e
Jonathan Nielsen <jonathan@jmnet.us>
2011-03-16 16:29:37 -0600

    Added color themes!  Pretty!

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
 ; first file to be loaded.
 (setq backup-inhibited t)
+(add-to-list 'load-path "~/.emacs.d/")
 (load-file "~/.emacs.d/indent-options.el")
 (load-file "~/.emacs.d/run-current-file.el")
+
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-arjen)
